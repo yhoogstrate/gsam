e2990b6724bbf48ff643d85d3d65d03bff4cea7e
yhoogstrate <y.hoogstrate@gmail.com>
2019-09-20 13:21:33 +0200

    sav

--- scripts/plot_v3_reads.py
+++ scripts/plot_v3_reads.py
@@ -3,46 +3,27 @@
 idx = {}
 
 import os
+from tqdm import tqdm
 
-print("sid\t\tresection-1-wt\tresection-1-v3\t\tresection-2-wt\tresection-2-v3")
+with open("output/tables/v3.txt", "w") as fh_out:
+    fh_out.write("sid\t\tresection-1-wt\tresection-1-v3\t\tresection-2-wt\tresection-2-v3\n")
 
-"""
-path = "/mnt/galaxian/data/users/youri/neurogen-tmp/align-gsam/v3/"
-for _ in os.listdir(path):
-    with open(path + _ , "r") as fh:
-        for line in fh:
+    with open("output/tables/v3_dedup.txt") as fh:
+        for line in tqdm(fh):
             line = line.strip()
-            if len(line) != "":
+            if len(line) > 0:
                 line = line.split("\t")
                 if line[0] != "sample":
-                    sid = _.split("_")
-                    resection = sid[1][3]
-                    sid = sid[1][0:3]
-                    
+                    sid = line[0].split("Pfrench_")[1].split("_")[0]
+                    resection = sid[3]
+                    sid = sid[0:3]
+
                     if sid not in idx:
                         idx[sid] = {'1': ["NA", "NA"], '2': ["NA", "NA"]}
 
                     idx[sid][resection] = line[1:]
-"""
 
 
-with open("output/tables/v3_dedup.txt") as fh:
-    for line in fh:
-        line = line.strip()
-        if len(line) > 0:
-            line = line.split("\t")
-            if line[0] != "sample":
-                sid = line[0].split("Pfrench_")[1].split("_")[0]
-                resection = sid[3]
-                sid = sid[0:3]
+    for sid in sorted(idx.keys()):
+        fh_out.write(sid + "\t|\t" + idx[sid]["1"][0] + "\t" + idx[sid]["1"][1] + "\t|\t" +  idx[sid]["2"][0] + "\t" + idx[sid]["2"][1] + "\n")
 
-                if sid not in idx:
-                    idx[sid] = {'1': ["NA", "NA"], '2': ["NA", "NA"]}
-
-                idx[sid][resection] = line[1:]
-
-
-for sid in sorted(idx.keys()):
-    print (sid + "\t|\t" + idx[sid]["1"][0] + "\t" + idx[sid]["1"][1] + "\t|\t" +  idx[sid]["2"][0] + "\t" + idx[sid]["2"][1])
-
-#print(ids[1][0:3] + "\t" + ids[1][3] + "\t" + "\t".join(line[1:]))
