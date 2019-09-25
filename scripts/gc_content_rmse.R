ddec5fae3c9c769a6a034d8b37746ef743adb891
yhoogstrate <y.hoogstrate@gmail.com>
2019-09-25 15:35:32 +0200

    sav

--- scripts/gc_content_rmse.R
+++ scripts/gc_content_rmse.R
@@ -10,6 +10,7 @@ e <- d[order(d$RMSE, d$sample.id),]
 # 25.11 24.61 25.61 24.67
 
 
+png("output/figures/gc_content_rmse.png", width=480*4,heigh=480*2,res=72*2)
 plot(c(1,nrow(e)), c(0,50), type="n", ylab="",xlab="Sample nr. / order",main="GC deviation")
 
 for(i in 1:nrow(e)) {
@@ -28,8 +29,8 @@ points(1:nrow(e) , e$percentage.C, col=rgb(0.5,0.5,0),pch="-", cex=1.5)
 points(1:nrow(e) , e$percentage.T, col=rgb(0.5,0,0.5),pch="-", cex=1.5)
 points(1:nrow(e) , e$percentage.G, col="blue",pch="-", cex=1.5)
 
-text(1:nrow(e) - 0.8,  45, e$sample.id , pos=4, srt=90, cex=0.65)
+text(1:nrow(e) - 0.8,  45, e$sample.id , pos=4, srt=90, cex=0.75)
 
-legend(0, 20, pch=c("-","-","-","-","o"), col=c(rgb(0,0.5,0.0),rgb(0.5,0.5,0),rgb(0.5,0,0.5),"blue","black"), c("%A","%C","%T","%G","RMSE from 25%") , cex=0.9)
+legend(0, 20, pch=c("-","-","-","-","o"), col=c(rgb(0,0.5,0.0),rgb(0.5,0.5,0),rgb(0.5,0,0.5),"blue","black"), c("%A","%C","%T","%G","RMSE from 25%") )
 
-
+dev.off()
