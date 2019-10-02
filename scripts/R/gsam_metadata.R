e38a1af99809a8aa2200c978c9d5c7455ab2dc62
yhoogstrate <y.hoogstrate@erasmusmc.nl>
2019-10-02 14:55:26 +0200

    sav

--- scripts/R/gsam_metadata.R
+++ scripts/R/gsam_metadata.R
@@ -1,20 +1,22 @@
 #!/usr/bin/env R
 
+library(dplyr) # for distinct() function
+
 # duplicate reads
-gsam.metadata <- read.delim("output/tables/duplicate_reads_sambamba_stats.txt",header=T,sep="\t",stringsAsFactors = F)
+gsam.metadata <- read.delim("data/RNA/output/tables/duplicate_reads_sambamba_stats.txt",header=T,sep="\t",stringsAsFactors = F)
 colnames(gsam.metadata)[2:ncol(gsam.metadata)] <- paste0("sambamba.",colnames(gsam.metadata)[2:ncol(gsam.metadata)])
 gsam.metadata$pid <- gsub("^([A-Z]+)[0-9]+$","\\1",gsam.metadata$sample)
 gsam.metadata$resection <- as.factor(gsub("^[A-Z]+([0-9]+)$","\\1",gsam.metadata$sample))
 
 # low complexity reads
-tmp <- read.delim("output/tables/low_complexity_reads.txt",header=T,sep="\t",stringsAsFactors = F)
+tmp <- read.delim("data/RNA/output/tables/low_complexity_reads.txt",header=T,sep="\t",stringsAsFactors = F)
 tmp$percentage.low.complexity.reads <- as.numeric(gsub("%","",tmp$percentage.low.complexity.reads,fixed=T))
 colnames(tmp)[2:ncol(tmp)] <- paste0("fastp.",colnames(tmp)[2:ncol(tmp)])
 gsam.metadata <- merge(gsam.metadata, tmp,by.x="sample.id", by.y = "sample")
 rm(tmp)
 
 # mapping stats STAR
-tmp <- read.delim("output/tables/star_percentage_mapped.txt",header=T,sep="\t",stringsAsFactors = F)
+tmp <- read.delim("data/RNA/output/tables/star_percentage_mapped.txt",header=T,sep="\t",stringsAsFactors = F)
 tmp$percentage.uniquely.mapped <- as.numeric(gsub("%","",tmp$percentage.uniquely.mapped,fixed=T))
 tmp$percentage.multimap <- as.numeric(gsub("%","", tmp$percentage.multimap,fixed=T))
 colnames(tmp)[2:ncol(tmp)] <- paste0("star.",colnames(tmp)[2:ncol(tmp)])
@@ -22,7 +24,7 @@ gsam.metadata <- merge(gsam.metadata, tmp,by.x="sample.id", by.y = "sample")
 rm(tmp)
 
 # featureCounts stats
-tmp <- read.delim("output/tables/featureCounts_gsam_1st96.exon-level.txt.summary",header=T,sep="\t",stringsAsFactors = F,row.names=1)
+tmp <- read.delim("data/RNA/output/tables/featureCounts_gsam_1st96.exon-level.txt.summary",header=T,sep="\t",stringsAsFactors = F,row.names=1)
 colnames(tmp) <- gsub("^[^_]+_([^_]+)_.+$","\\1",colnames(tmp))
 tmp <- t(tmp) # transpose
 tmp <- data.frame(tmp)
@@ -35,13 +37,13 @@ gsam.metadata <- merge(gsam.metadata, tmp, by.x="sample.id", by.y = "sample")
 rm(tmp, sel)
 
 # gc 
-tmp <- read.delim("output/tables/gc_content_rmse.txt")
+tmp <- read.delim("data/RNA/output/tables/gc_content_rmse.txt")
 colnames(tmp)[2:ncol(tmp)] <- paste0("gc.control.",colnames(tmp)[2:ncol(tmp)])
 gsam.metadata <- merge(gsam.metadata, tmp, by.x="sample.id", by.y = "sample.id")
 rm(tmp)
 
 # egfr-v3 (RNA-seq)
-tmp <- read.delim("output/tables/v3_extract_readcounts.txt")
+tmp <- read.delim("data/RNA/output/tables/v3_extract_readcounts.txt")
 tmp$sample <- gsub("^[^_]+_([^_]+)_.+$","\\1",tmp$sample)
 tmp$v3.percentage <- tmp$vIII.reads.v3 / (tmp$vIII.reads.v3 + tmp$wt.reads.v3) * 100.0
 tmp[(tmp$vIII.reads.v3 + tmp$wt.reads.v3) <= 6,]$v3.percentage <- NA
