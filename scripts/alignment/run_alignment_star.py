a8afa2d5c1180b67ab832fa2326e32a3379ec784
yhoogstrate <y.hoogstrate@erasmusmc.nl>
2019-10-22 11:36:39 +0200

    sav

--- scripts/alignment/run_alignment_star.py
+++ scripts/alignment/run_alignment_star.py
@@ -10,6 +10,8 @@ import re
 idx = {}
 
 with open("scripts/alignment/run_alignment_star.sh", "w") as fh:
+	fh.write("#!/bin/bash\n\n")
+	
 	for _ in sorted([_ for _ in os.listdir("data2/fastq-clean") if _[-9:] == ".fastq.gz"]):
 		#sid = _.split("-",1)[0]
 		sid = re.split("\-[0-9]+_NGS19", _)[0]
@@ -26,15 +28,87 @@ with open("scripts/alignment/run_alignment_star.sh", "w") as fh:
 		if sid not in idx[pid]:
 			idx[pid][sid] = {}
 		
-		idx[pid][sid][srid] = {'R1': None, 'R2': None}
-		
-		
-		#print(pid, sid, res, srid, mate)
+		if not srid in idx[pid][sid]:
+			idx[pid][sid][srid] = {'R1': None, 'R2': None}
 
-for pid in sorted(idx):
-	print(pid)
-	for sid in sorted(idx[pid]):
-		print("  " + sid + ":")
-		for srid in sorted(idx[pid][sid]):
-			print("    " + srid)
-	print("")
+		idx[pid][sid][srid][mate] = _
+		
+
+
+	for pid in sorted(idx):
+		print(pid)
+		for sid in sorted(idx[pid]):
+			print("  " + sid + ":")
+			outdir = "processed/" + sid
+			
+			fh.write('mkdir -p "' + outdir + '";\n')
+			fh.write('nice ./bin/STAR/bin/Linux_x86_64/STAR \\\n')
+			fh.write('    --outFileNamePrefix "' + outdir + '" \\\n')
+			fh.write('    --runThreadN 84 \\\n')
+			fh.write('    --genomeDir ref/star-hg19 \\\n')
+			fh.write('    --sjdbGTFfile ref/star-hg19/gencode.v31lift37.annotation.gtf \\\n')
+			fh.write('    --readFilesIn \\\n')
+			r1 = []
+			r2 = []
+			
+			for srid in sorted(idx[pid][sid]):
+				if idx[pid][sid][srid]['R1'] is not None and idx[pid][sid][srid]['R2'] is not None:
+					r1.append('data2/fastq-clean/' + idx[pid][sid][srid]['R1'])
+					r2.append('data2/fastq-clean/' + idx[pid][sid][srid]['R2'])
+					#print("    " + srid + "\t\tCOMPLETE")
+				else:
+					print("    " + srid + "\t\tINCOMPLETE !!!!!!!!!!!!!!!")
+					raise Exception("???")
+
+			fh.write('        ' + ','.join(r1) + ' \\\n')
+			fh.write('        ' + ','.join(r2) + ' \\\n')
+			fh.write('    --readFilesCommand zcat \\\n')
+			fh.write('    --outSAMtype BAM SortedByCoordinate \\\n')
+			fh.write('    --alignSJoverhangMin 10 \\\n')
+			fh.write('    --alignSJDBoverhangMin 1 \\\n')
+			fh.write('    --alignIntronMin 20 \\\n')
+			fh.write('    --alignIntronMax 1000000 \\\n')
+			fh.write('    --alignMatesGapMax 1000000 \\\n')
+			fh.write('    --chimSegmentMin 12 \\\n')
+			fh.write('    --chimJunctionOverhangMin 12 \\\n')
+			fh.write('    --chimOutType WithinBAM SeparateSAMold \\\n')
+			fh.write('    --outSAMstrandField intronMotif \\\n')
+			fh.write('    --outFilterIntronMotifs RemoveNoncanonicalUnannotated \\\n')
+			fh.write('    --quantMode TranscriptomeSAM\n')
+
+			fh.write('\n\n')
+		print("")
+		fh.write('\n# - - - - -\n\n')
+
+	"""
+
+	mkdir -p "bam/Pfrench_EBU1_2627_Plate1_G8_S117_001_hg19/"
+	nice ./bin/STAR/bin/Linux_x86_64/STAR \
+		--runThreadN 84 \
+		--genomeDir /data/users/youri/neurogen-tmp/align-gsam/star-hg19 \
+		--sjdbGTFfile ref/star-hg19/star-hg19/gencode.v31lift37.annotation.gtf \
+		--readFilesIn /dev/shm/Pfrench_EBU1_2627_Plate1_G8_S117_001.dedup.clean_R1.fastq.gz \
+					  /dev/shm/Pfrench_EBU1_2627_Plate1_G8_S117_001.dedup.clean_R2.fastq.gz \
+		--readFilesCommand zcat \
+		--outSAMtype BAM SortedByCoordinate \
+		--outFileNamePrefix "bam/Pfrench_EBU1_2627_Plate1_G8_S117_001_hg19/" \
+		--alignSJoverhangMin 10 \
+		--alignSJDBoverhangMin 1 \
+		--alignIntronMin 20 \
+		--alignIntronMax 1000000 \
+		--alignMatesGapMax 1000000 \
+		--chimSegmentMin 12 \
+		--chimJunctionOverhangMin 12 \
+		--chimOutType WithinBAM SeparateSAMold \
+		--outSAMstrandField intronMotif \
+		--outFilterIntronMotifs RemoveNoncanonicalUnannotated \
+		--quantMode TranscriptomeSAM
+
+
+
+	With 
+
+		--quantMode TranscriptomeSAM
+
+	GeneCounts, and get both the Aligned.toTranscriptome.out.bam and ReadsPerGene.out.tab outputs.
+	"""
