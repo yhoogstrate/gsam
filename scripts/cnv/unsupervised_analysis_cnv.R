391d6d6321d5cf87612430b85794f69cb6e1d590
yhoogstrate <y.hoogstrate@gmail.com>
2019-10-23 13:22:37 +0200

    saav

--- scripts/cnv/unsupervised_analysis_cnv.R
+++ scripts/cnv/unsupervised_analysis_cnv.R
@@ -1,23 +1,22 @@
 #!/usr/bin/env R
 
+setwd("~/projects/gsam")
+
 # there is a difference in size of results files of CNVKit
 # they either have 27493 lines (all in ExtraSequencing_CN_data_29Mar2018/)
 # or have 27287 lines (195/374) = 50%
 
 # @todo: synch with neuro-genomic and change data directory
 
-d <- read.table("output/tables/cnv_copynumber-ratio.cnr_all.txt")
+d <- read.table("output/tables/cnv_copynumber-ratio.cnr_all.txt",stringsAsFactors = F,header=T)
+naPerRow <- apply(d, 1, function(x) sum(is.na(x)))
+x <- nrow(d)
+# there are four samples of which the CNV data is missing at this moment, and are always NA
+d <- d[naPerRow <= 4,]
+print(paste0("Removal of ",x - nrow(d) , " CNV regions due to batch effects in files" ))
 
-#test_27493 <- read.table("data/DNA/cnvkit/first_series/PD36773c__tumour.cnr", header=T)
-#test_27287 <- read.table("/home/yhoogstrate/mnt/neuro-genomic-ro/gsam/DNA/cnvkit/first_series/PD29177a2__tumour.cnr" ,header=T)
-#t1 <- test_27493
-#t2 <- test_27287
-#t1$loc <- paste0(t1$chromosome,":",t1$start,"-",t1$end)
-#t2$loc <- paste0(t2$chromosome,":",t2$start,"-",t2$end)
-#isct <- intersect(t1$loc, t2$loc)
-#length(isct)
+genes <- d[,1:4]
+d <- d[,-(1:4)]
 
-
-
-
+dd <-
 
