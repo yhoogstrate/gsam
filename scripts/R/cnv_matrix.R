01affd968548fef9cbdda8a8dc275ca5442752e4
yhoogstrate <y.hoogstrate@erasmusmc.nl>
2019-10-24 11:20:58 +0200

    nice parsing function for cnv data

--- scripts/R/cnv_matrix.R
+++ scripts/R/cnv_matrix.R
@@ -2,22 +2,36 @@
 
 
 cnv_matrix <- read.table("data/output/tables/cnv_copynumber-ratio.cnr_log2_all.txt",stringsAsFactors = F,header=T)
+
+# make a separate table for the gene information
+cnv_matrix_genes <- cnv_matrix[,1:4]
+cnv_matrix_genes$chr <- paste0('chr',cnv_matrix_genes$chromosome)
+cnv_matrix_genes$loc <- paste0(cnv_matrix_genes$chr,":",cnv_matrix_genes$start, "-", cnv_matrix_genes$end)
+cnv_matrix <- cnv_matrix[,-(1:4)] # exclude gene details
+rownames(cnv_matrix) <- cnv_matrix_genes$loc
+
+# reorder by sorted colnames
+cnv_matrix <- cnv_matrix[,order(colnames(cnv_matrix))]
+
+
+
+# remove rows with NA values
 naPerRow <- apply(cnv_matrix, 1, function(tmp) sum(is.na(tmp)))
 tmp <- nrow(cnv_matrix)
-# there are four samples of which the CNV data is missing at this moment, and are always NA
-cnv_matrix <- cnv_matrix[naPerRow <= 4,]
+sel <- naPerRow <= 4
+cnv_matrix <- cnv_matrix[sel,]
+cnv_matrix_genes <- cnv_matrix_genes[sel,]
 print(paste0("Removal of ",tmp - nrow(cnv_matrix) , " CNV regions only present in a subset of samples, because of different batches?" ))
 
+
+
+# remove columns with NA values
 naPerCol <- apply(cnv_matrix, 2, function(x) sum(is.na(x)))
 tmp <- ncol(cnv_matrix)
 cnv_matrix <- cnv_matrix[,naPerCol == 0]
 print(paste0("Removal of ",tmp - ncol(cnv_matrix) , " CNV regions due to missing/corrupt and excluded files" ))
 
-rm(tmp, naPerRow, naPerCol)
 
-# make a separate table for the gene information
-cnv_matrix_genes <- cnv_matrix[,1:4]
-cnv_matrix_genes$chr <- paste0('chr',cnv_matrix_genes$chromosome)
-cnv_matrix <- cnv_matrix[,-(1:4)] # exclude gene details
-rownames(cnv_matrix) <- paste0(cnv_matrix_genes$chr,":",cnv_matrix_genes$start, "-", cnv_matrix_genes$end)
+
+rm(tmp, naPerRow, naPerCol, sel)
 
