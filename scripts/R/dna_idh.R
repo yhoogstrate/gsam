f4004a4dacb0cdb23c87999a2ba08ca00aec6b8d
yhoogstrate <y.hoogstrate@gmail.com>
2019-11-01 10:07:33 +0100

    sav idh

--- scripts/R/dna_idh.R
+++ scripts/R/dna_idh.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env R
 
 dna_idh <- read.delim("data/DNA/GBM_allSamples_PassVariants_1736_GBM_TERT_05Jan2018_flags_VAFs.txt",stringsAsFactors=F,sep="\t")
+dna_idh_tested <- data.frame(Sample=unique(dna_idh$Sample))
 dna_idh$Normal <- NULL
 dna_idh$Sample <- dna_idh$Sample
 dna_idh <- dna_idh[ dna_idh$Gene %in% c("IDH1", "IDH2") ,]
@@ -19,17 +20,20 @@ dna_idh <- dna_idh[is_idh1 | is_idh2  ,] # n=18
 
 rm(is_idh1, is_idh2)
 
-is_idh <- as.character(dna_idh$Sample)
-is_idh <- unique(c(is_idh, c("PD29228a","PD29228c","PD29263a","PD29264a2","PD29264c","PD30239c","PD30242a3","PD30242c3","PD36768a","PD36768c","PD36770a","PD36770c","PD36772c","PD36783a","PD36783c")))
 
+dna_idh <- dna_idh[,colnames(dna_idh) %in% c("Sample", "ID", "ShriramVAF")]
+#print(dim(dna_idh))
 
-tmp <- read.delim("data/DNA/sample codes sanger gsam.txt",stringsAsFactors=FALSE)
-tmp <- tmp[,match(c("donor_ID", "PD_ID"), colnames(tmp))]
+dna_idh <- merge(dna_idh_tested, dna_idh, by.x = "Sample", by.y = "Sample" , all.x=T)
+rm(dna_idh_tested)
+#print(dim(dna_idh_tested))
+#print(dim(dna_idh))
 
+dna_idh[is.na(dna_idh$ID),]$ID <- "-"
+dna_idh[is.na(dna_idh$ShriramVAF),]$ShriramVAF <- "-"
+colnames(dna_idh) <- c("Sample","IDH.mut", "IDH.mut.VAF")
 
-match(is_idh  , tmp$PD_ID)
-is_idh <- tmp[match(is_idh  , tmp$PD_ID),]$donor_ID
-
-rm(tmp, dna_idh)
-
+tmp <- gsam.cnv.metadata[,colnames(gsam.cnv.metadata) %in% c("sid","donor_ID")]
+dna_idh <- merge(dna_idh, tmp, by.x="Sample", by.y = "sid", all.x=TRUE)
+rm(tmp)
 
