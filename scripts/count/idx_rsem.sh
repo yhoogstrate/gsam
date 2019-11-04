3e081b69ca72a7335910c7a6d53342367d2e4267
yhoogstrate <y.hoogstrate@gmail.com>
2019-11-04 10:20:19 +0100

    sav

---
+++ scripts/count/idx_rsem.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rsem-prepare-reference \
+    --gtf /mnt/data/ccbc_environment/project/gsam-neurology/ref/star-hg19/gencode.v31lift37.annotation.gtf \
+    /mnt/data/ccbc_environment/project/gsam-neurology/ref/star-hg19/hg19.fa \
+    /mnt/data/ccbc_environment/project/gsam-neurology/ref/rsem-hg19/gencode.v31
+
+
+
