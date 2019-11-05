3cc4716c41cd2dd2f26de1a6560ff98a817b7246
yhoogstrate <y.hoogstrate@erasmusmc.nl>
2019-11-05 16:33:37 +0100

    sav

--- scripts/R/gsam_metadata.R
+++ scripts/R/gsam_metadata.R
@@ -34,26 +34,28 @@ gsam.cnv.metadata$donor_sex <- NULL # incomplete, the one from patient metadata 
 # --- RNA-seq metadata ----
 
 # duplicate reads
-gsam.metadata <- read.delim("data/RNA/output/tables/duplicate_reads_sambamba_stats.txt",header=T,sep="\t",stringsAsFactors = F)
-colnames(gsam.rna.metadata)[2:ncol(gsam.rna.metadata)] <- paste0("sambamba.",colnames(gsam.rna.metadata)[2:ncol(gsam.rna.metadata)])
-gsam.rna.metadata$pid <- gsub("^([A-Z]+)[0-9]+$","\\1",gsam.rna.metadata$sample)
-gsam.rna.metadata$resection <- as.factor(gsub("^[A-Z]+([0-9]+)$","\\1",gsam.rna.metadata$sample))
+gsam.metadata <- read.delim("data/RNA/output/tables/duplicate_reads_sambamba_stats.txt",header=T,sep="\t",stringsAsFactors=F)
+colnames(gsam.metadata)[2:ncol(gsam.metadata)] <- paste0("sambamba.",colnames(gsam.metadata)[2:ncol(gsam.metadata)])
+gsam.metadata$pid <- gsub("^([A-Z]+)[0-9]+$","\\1",gsam.metadata$sample)
+gsam.metadata$resection <- as.factor(gsub("^[A-Z]+([0-9]+)$","\\1",gsam.metadata$sample))
 
 
 # low complexity reads
 tmp <- read.delim("data/RNA/output/tables/low_complexity_reads.txt",header=T,sep="\t",stringsAsFactors = F)
 tmp$percentage.low.complexity.reads <- as.numeric(gsub("%","",tmp$percentage.low.complexity.reads,fixed=T))
 colnames(tmp)[2:ncol(tmp)] <- paste0("fastp.",colnames(tmp)[2:ncol(tmp)])
-gsam.metadata <- merge(gsam.rna.metadata, tmp,by.x="sample.id", by.y = "sample")
+gsam.metadata <- merge(gsam.metadata, tmp,by.x="sample.id", by.y = "sample")
 rm(tmp)
+
 
 # mapping stats STAR
 tmp <- read.delim("data/RNA/output/tables/star_percentage_mapped.txt",header=T,sep="\t",stringsAsFactors = F)
 tmp$percentage.uniquely.mapped <- as.numeric(gsub("%","",tmp$percentage.uniquely.mapped,fixed=T))
 tmp$percentage.multimap <- as.numeric(gsub("%","", tmp$percentage.multimap,fixed=T))
 colnames(tmp)[2:ncol(tmp)] <- paste0("star.",colnames(tmp)[2:ncol(tmp)])
-gsam.metadata <- merge(gsam.rna.metadata, tmp,by.x="sample.id", by.y = "sample")
+gsam.metadata <- merge(gsam.metadata, tmp,by.x="sample.id", by.y = "sample")
 rm(tmp)
+
 
 # featureCounts stats
 tmp <- read.delim("data/RNA/output/tables/featureCounts_gsam_1st96.exon-level.txt.summary",header=T,sep="\t",stringsAsFactors = F,row.names=1)
@@ -65,14 +67,16 @@ tmp$sample <-  rownames(tmp)
 tmp$percentage.exon.counts <- tmp$Assigned/ tmp$read.count * 100
 sel <- colnames(tmp) != "sample"
 colnames(tmp)[sel] <- paste0("featurecounts.",colnames(tmp)[sel])
-gsam.metadata <- merge(gsam.rna.metadata, tmp, by.x="sample.id", by.y = "sample")
+gsam.metadata <- merge(gsam.metadata, tmp, by.x="sample.id", by.y = "sample")
 rm(tmp, sel)
+
 
 # gc 
 tmp <- read.delim("data/RNA/output/tables/gc_content_rmse.txt")
 colnames(tmp)[2:ncol(tmp)] <- paste0("gc.control.",colnames(tmp)[2:ncol(tmp)])
-gsam.metadata <- merge(gsam.rna.metadata, tmp, by.x="sample.id", by.y = "sample.id")
+gsam.metadata <- merge(gsam.metadata, tmp, by.x="sample.id", by.y = "sample.id")
 rm(tmp)
+
 
 # egfr-v3 (RNA-seq)
 tmp <- read.delim("data/RNA/output/tables/v3_extract_readcounts.txt")
@@ -80,48 +84,14 @@ tmp$sample <- gsub("^[^_]+_([^_]+)_.+$","\\1",tmp$sample)
 tmp$v3.percentage <- tmp$vIII.reads.v3 / (tmp$vIII.reads.v3 + tmp$wt.reads.v3) * 100.0
 tmp[(tmp$vIII.reads.v3 + tmp$wt.reads.v3) <= 6,]$v3.percentage <- NA
 colnames(tmp)[2:ncol(tmp)] <- paste0("v3.rnaseq.",colnames(tmp)[2:ncol(tmp)])
-gsam.metadata <- merge(gsam.rna.metadata, tmp, by.x="sample.id", by.y = "sample")
+gsam.metadata <- merge(gsam.metadata, tmp, by.x="sample.id", by.y = "sample")
 rm(tmp)
 
-# add gender and age
-#tmp <- read.delim("data/DNA/sample codes sanger gsam.txt",stringsAsFactors=FALSE)
-#tmp <- tmp[,match(c("donor_ID", "donor_sex", "donor_age_at_diagnosis"), colnames(tmp))]
-#tmp[tmp$donor_sex == "",]$donor_sex <- NA
-#tmp$donor_age_at_diagnosis <- as.integer(round(tmp$donor_age_at_diagnosis ))
-#tmp <- tmp[order(tmp$donor_ID),]
-
-## 'clever' overwrite inconsistent duplicates with EXACT same entry, and collapse later with 'distinct' function
-#for(sid in unique(tmp$donor_ID)) {
-#    tmp.sid <- tmp[tmp$donor_ID == sid,]
-#    tmp.sid.g <- tmp[tmp$donor_ID == sid & !is.na(tmp$donor_sex),]
-    
-#    if( nrow(tmp.sid.g) == 0 ){
-#        tmp[tmp$donor_ID == sid,]$donor_sex = NA
-#    }
-#    else {
-#        tmp[tmp$donor_ID == sid,]$donor_sex = tmp.sid.g$donor_sex[1]
-#    }
-    
-#    tmp.sid.g <- tmp[tmp$donor_ID == sid & !is.na(tmp$donor_age_at_diagnosis),]
-#    if( nrow(tmp.sid.g) == 0 ){
-#        tmp[tmp$donor_ID == sid,]$donor_age_at_diagnosis = NA
-#    }
-#    else {
-#        tmp[tmp$donor_ID == sid,]$donor_age_at_diagnosis = mean(tmp.sid.g$donor_age_at_diagnosis)
-#    }
-    
-#    rm(tmp.sid, tmp.sid.g)
-#}
-
-#tmp <- distinct(tmp)
-
-#gsam.metadata <- merge(gsam.metadata , tmp , by.x="sample.id" , by.y = "donor_ID" , all.y = F, no.dups=T) # apparently there are dupes
-#rm(tmp, sid)
 
 
 # all metadata, but per patient and not per resection (collapsed)
 tmp <- read.delim("data/administratie/GSAM_combined_clinical_molecular.csv", sep="," , stringsAsFactors=F)
-gsam.metadata <- merge(gsam.rna.metadata, tmp, by.x="pid", by.y="studyID", all.x = T, all.y=F, no.dups=F)
+gsam.metadata <- merge(gsam.metadata, tmp, by.x="pid", by.y="studyID", all.x = T, all.y=F, no.dups=F)
 
 
 # --- RNA-seq metadata [full] ----
@@ -138,13 +108,14 @@ gsam.rna.metadata$resection <- as.factor(gsub("^.+([0-9])$","r\\1",rownames(gsam
 
 # @ todo add batch [1/2]
 
-# add IDH
-tmp <- dna_idh[dna_idh$duplicate == FALSE,]
-tmp$Sample <- NULL
-tmp$duplicate <- NULL
-print(dim(gsam.rna.metadata))
-gsam.rna.metadata <- merge(gsam.rna.metadata, tmp, by.x = "sid", by.y = "donor_ID", all.x = TRUE)
-print(dim(gsam.rna.metadata))
+## add IDH
+#source("scripts/R/dna_idh.R")
+#tmp <- dna_idh[dna_idh$duplicate == FALSE,]
+#tmp$Sample <- NULL
+#tmp$duplicate <- NULL
+#print(dim(gsam.rna.metadata))
+#gsam.rna.metadata <- merge(gsam.rna.metadata, tmp, by.x = "sid", by.y = "donor_ID", all.x = TRUE)
+#print(dim(gsam.rna.metadata))
 
 
 
--- scripts/rna/idh_cluster_full_rna_data.R
+++ scripts/rna/idh_cluster_full_rna_data.R
@@ -20,8 +20,8 @@ get_ensembl_hsapiens_gene_ids
 
 Loads a function that allows to translate ENSEMBL IDs into HUGO symbols (handy for sharing tables) and Entrez IDs (handy for GSEa like analysis)
 "
-source("scripts/R/ensembl_to_geneid.R") # obsolete? can be replaced with the get_ensembl function
-ensembl_genes <- get_ensembl_hsapiens_gene_ids()
+#source("scripts/R/ensembl_to_geneid.R") # obsolete? can be replaced with the get_ensembl function
+#ensembl_genes <- get_ensembl_hsapiens_gene_ids()
 
 
 # ---- load: data ----
@@ -96,54 +96,77 @@ cond <- as.factor(paste0("idh.",dna_idh[match(colnames(e3), dna_idh$donor_ID),]$
 dds <- DESeqDataSetFromMatrix(e3, DataFrame(cond), ~cond)
 e.vst3 <- assay(vst(dds,blind=T))
 e.vst3 <- e.vst3[match(rownames(high_variance_genes), rownames(e.vst3)),]
-pcn <- scale(t(e.vst3), pc$center, pc$scale) %*% pc$rotation
+pcn <- scale(t(e.vst3), pc$center, pc$scale) %*% pc$rotation # rescale new data onto pca of first 96 samples
 
+cond.idh <- as.factor(paste0("idh.",dna_idh[match(colnames(e3), dna_idh$donor_ID),]$IDH.mut != "-"))
+cond.chr10 <- as.factor(gsam.patient.metadata[match( gsub("[12]$","",rownames(pcn))   , gsam.patient.metadata$studyID   ),]$primary10)
+cond <- as.factor(paste0(cond.idh,".",cond.chr10))
 plot(pcn[,1], pcn[,2], cex=0.7,pch=19,col=as.numeric(cond)+1)
 
 
 
+tmp <- data.frame(
+  sid = colnames(e3),
+  pc1 = pcn[,1],  pc2 = pcn[,2],  pc3 = pcn[,3],  pc4 = pcn[,4],  pc5 = pcn[,5],  pc6 = pcn[,6],
+  resection = as.factor(gsub("^.+([1-2])$","\\1",colnames(e3))),
+  idh = as.factor(gsub("TRUE","mut",gsub("FALSE","wt",paste0("",dna_idh[match(colnames(e3), dna_idh$donor_ID),]$IDH.mut != "-")))),
+  chr10 = as.factor(gsam.patient.metadata[match( gsub("[12]$","",rownames(pcn)),
+                                                 gsam.patient.metadata$studyID   ),]$primary10),
+  chr7 = as.factor(gsam.patient.metadata[match( gsub("[12]$","",rownames(pcn)),
+                                                gsam.patient.metadata$studyID   ),]$primary7),
+  atrx = as.factor(gsam.patient.metadata[match( gsub("[12]$","",rownames(pcn)),
+                                                gsam.patient.metadata$studyID   ),]$ATRX),
+  gender = as.factor(gsam.patient.metadata[match( gsub("[12]$","",rownames(pcn))   , gsam.patient.metadata$studyID   ),]$gender) ,
+  
+  mgmt = as.factor(gsam.patient.metadata[match( gsub("[12]$","",rownames(pcn)),
+                                                gsam.patient.metadata$studyID   ),]$initialMGMT),
+  
+  tlc  = as.factor(gsam.patient.metadata[match( gsub("[12]$","",rownames(pcn))                              , gsam.patient.metadata$studyID   ),]$tumorLocation))
+tmp$chr7.wt.chr10.wt <- tmp$chr10 == "Normal" & tmp$chr7 == "Normal"
+
+
+# pca x co-amplificatie chr7&chr10
+ggplot(tmp, aes(x=pc1, y=pc2, col=chr7.wt.chr10.wt)) +
+  geom_point(size=2) +
+  labs(
+      title = paste0("PCA GSAM RNA normalised expression (",nrow(tmp)," samples)"),
+              fill = "Chr7-wt & Chr10-wt") +
+  job_gg_theme 
+ggsave("output/figures/rna/pca_rna_x_chr7_chr10_ampl.png")
+
+
+ggplot(tmp, aes(x=pc1, y=pc2, col=idh)) +
+  geom_point(size=2) +
+  ggtitle(paste0("PCA GSAM RNA normalised expression (",nrow(tmp)," samples)")) +
+  labs(fill = "IDH-mutation",
+       subtitle="*Only high confidence IDH mutations included") +
+  job_gg_theme 
+gsave("output/figures/rna/pca_rna_x_idh.png")
+
+
+ggplot(tmp, aes(x=pc1, y=pc2, col=atrx)) + geom_point(size=2)
+ggplot(tmp, aes(x=pc1, y=pc2, col=mgmt)) + geom_point(size=2)
 
 
 
 
-# ---- PCA: pc1 & pc2  x  loss10   ----
-png("output/figures/unspervised_expression_analysis_vst_pca_x_loss10.png",width=480*2,height=480*2,res=72*2)
-
-e <- expression_matrix
-
-stopifnot(sum(colnames(e) == gsam.metadata[match(colnames(e) , gsam.metadata$sample.id),]$sample.id) == ncol(e))
-#cond <- as.factor(gsub("[ \\+\\.\\/\\-]{1,}",".",gsam.metadata$primary710,fixed=F)) # chr7gain + chr10 loss
-#cond2 <- as.factor(gsub("FALSE","DNA.wt", gsub("TRUE","DNA.IDH",as.character(colnames(e) %in% is_idh)))) # idh
-cond <- as.factor(paste0("chr10.",gsam.metadata$primary10))
-
-dds <- DESeqDataSetFromMatrix(e, DataFrame(cond), ~cond)
-e.vst <- assay(vst(dds))
-
-ntop <- 500
-variances <- rowVars(e.vst)
-high_variance_genes <- e.vst[order(variances, decreasing = TRUE)[seq_len(min(ntop, length(variances)))],]
-
-pc <- prcomp(t(high_variance_genes))
-pc1 <- 1
-pc2 <- 2
-
-plot( c(min(pc$x[,pc1]), max(pc$x[,pc1])), c(min(pc$x[,pc2]) - 15,max(pc$x[,pc2])), type="n" , main=paste0("G-SAM RNA PCA (pc",pc1," & pc",pc2,")"),xlab=paste0("PCA pc-",pc1),ylab=paste0("PCA pc-",pc2))
-points(pc$x[,c(pc1,pc2)],  cex=0.7,pch=19,col=as.numeric(cond)+2)
 
 
-legend("bottomright", unique(as.character(cond)),
-       col=unique(as.numeric(cond) + 2),pch=19)
+
+# location and lowgrade is correlated
+#plot(tmp$tlc, tmp$chr7)
+#plot(tmp$tlc, tmp$idh)
 
 
 
 
+df <- data.frame(
+  c10=as.factor(gsam.patient.metadata$primary10),
+  c710=as.factor(gsam.patient.metadata$primary710
+                )
+  )
+df <- df[!is.na(df$c10) & !is.na(df$c710),]
 
-
-
-rm(pc, pc1, pc2, pch, cond , cond2, ntop, e, e.vst, dds, high_variance_genes, variances)
-
-
-dev.off()
 
 
 
