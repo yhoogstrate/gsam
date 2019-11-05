d257ff611fa12f3f36585b0ea7247738b56e8c75
yhoogstrate <y.hoogstrate@erasmusmc.nl>
2019-11-05 11:32:53 +0100

    sav

--- scripts/count/idx_rsem.sh
+++ scripts/count/idx_rsem.sh
@@ -2,7 +2,7 @@
 
 rsem-prepare-reference \
     --gtf /mnt/data/ccbc_environment/project/gsam-neurology/ref/star-hg19/gencode.v31lift37.annotation.gtf \
-    /mnt/data/ccbc_environment/project/gsam-neurology/ref/star-hg19/hg19.fa \
+    /mnt/data/ccbc_environment/project/gsam-neurology/ref/rsem-hg19/GRCh37.primary_assembly.genome.fa \
     /mnt/data/ccbc_environment/project/gsam-neurology/ref/rsem-hg19/gencode.v31
 
 
