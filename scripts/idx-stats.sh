4dd7c672d5f28a8131434101dd3aee2eb24ce1ae
yhoogstrate <y.hoogstrate@erasmusmc.nl>
2020-09-23 10:20:27 +0200

    updates

---
+++ scripts/idx-stats.sh
@@ -0,0 +1,30 @@
+#usr/bin
+
+samtools idxstats AAB2/Aligned.sortedByCoord.out.bam > AAB2.idxstats.txt 
+samtools idxstats AAP1/Aligned.sortedByCoord.out.bam > AAP1.idxstats.txt 
+samtools idxstats AZE1/Aligned.sortedByCoord.out.bam > AZE1.idxstats.txt 
+samtools idxstats AZH2/Aligned.sortedByCoord.out.bam > AZH2.idxstats.txt 
+samtools idxstats BAI2/Aligned.sortedByCoord.out.bam > BAI2.idxstats.txt 
+samtools idxstats BAT2/Aligned.sortedByCoord.out.bam > BAT2.idxstats.txt 
+samtools idxstats BAY2/Aligned.sortedByCoord.out.bam > BAY2.idxstats.txt
+samtools idxstats CAC1/Aligned.sortedByCoord.out.bam > CAC1.idxstats.txt
+samtools idxstats CAO1/Aligned.sortedByCoord.out.bam > CAO1.idxstats.txt
+samtools idxstats CAV1/Aligned.sortedByCoord.out.bam > CAV1.idxstats.txt
+samtools idxstats CBQ1/Aligned.sortedByCoord.out.bam > CBQ1.idxstats.txt
+samtools idxstats CBS1/Aligned.sortedByCoord.out.bam > CBS1.idxstats.txt
+samtools idxstats CCZ1/Aligned.sortedByCoord.out.bam > CCZ1.idxstats.txt
+samtools idxstats CDH1/Aligned.sortedByCoord.out.bam > CDH1.idxstats.txt
+samtools idxstats GAE1/Aligned.sortedByCoord.out.bam > GAE1.idxstats.txt
+samtools idxstats GAM1/Aligned.sortedByCoord.out.bam > GAM1.idxstats.txt
+samtools idxstats GAO2/Aligned.sortedByCoord.out.bam > GAO2.idxstats.txt
+samtools idxstats HAA2/Aligned.sortedByCoord.out.bam > HAA2.idxstats.txt
+samtools idxstats GAS1/Aligned.sortedByCoord.out.bam > GAS1.idxstats.txt
+samtools idxstats HAE1/Aligned.sortedByCoord.out.bam > HAE1.idxstats.txt
+samtools idxstats HAF1/Aligned.sortedByCoord.out.bam > HAF1.idxstats.txt
+samtools idxstats HAF2/Aligned.sortedByCoord.out.bam > HAF2.idxstats.txt
+samtools idxstats JAN1/Aligned.sortedByCoord.out.bam > JAN1.idxstats.txt
+samtools idxstats KAA1/Aligned.sortedByCoord.out.bam > KAA1.idxstats.txt
+samtools idxstats KAC1/Aligned.sortedByCoord.out.bam > KAC1.idxstats.txt
+samtools idxstats KAC2/Aligned.sortedByCoord.out.bam > KAC2.idxstats.txt
+samtools idxstats KAE1/Aligned.sortedByCoord.out.bam > KAE1.idxstats.txt
+
---
+++ scripts/markdedup-3rd.sh
@@ -0,0 +1,83 @@
+#usr/bin
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 AAP1/Aligned.sortedByCoord.out.bam AAP1/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 AZE1/Aligned.sortedByCoord.out.bam AZE1/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 AZH2/Aligned.sortedByCoord.out.bam AZH2/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 BAI2/Aligned.sortedByCoord.out.bam BAI2/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 BAT2/Aligned.sortedByCoord.out.bam BAT2/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 BAY2/Aligned.sortedByCoord.out.bam BAY2/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 CAC1/Aligned.sortedByCoord.out.bam CAC1/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 CAO1/Aligned.sortedByCoord.out.bam CAO1/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 CAV1/Aligned.sortedByCoord.out.bam CAV1/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 CBQ1/Aligned.sortedByCoord.out.bam CBQ1/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 CBS1/Aligned.sortedByCoord.out.bam CBS1/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 CCZ1/Aligned.sortedByCoord.out.bam CCZ1/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 CDH1/Aligned.sortedByCoord.out.bam CDH1/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 GAE1/Aligned.sortedByCoord.out.bam GAE1/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 GAM1/Aligned.sortedByCoord.out.bam GAM1/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 GAO2/Aligned.sortedByCoord.out.bam GAO2/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 GAS1/Aligned.sortedByCoord.out.bam GAS1/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 HAA2/Aligned.sortedByCoord.out.bam HAA2/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 HAE1/Aligned.sortedByCoord.out.bam HAE1/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 HAF1/Aligned.sortedByCoord.out.bam HAF1/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 HAF2/Aligned.sortedByCoord.out.bam HAF2/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 JAN1/Aligned.sortedByCoord.out.bam JAN1/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 KAA1/Aligned.sortedByCoord.out.bam KAA1/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 KAC1/Aligned.sortedByCoord.out.bam KAC1/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 KAC2/Aligned.sortedByCoord.out.bam KAC2/Aligned.sortedByCoord.out.markdup.bam
+nice sambamba markdup -t 3 --overflow-list-size 1000000 --hash-table-size 1000000 KAE1/Aligned.sortedByCoord.out.bam KAE1/Aligned.sortedByCoord.out.markdup.bam
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
