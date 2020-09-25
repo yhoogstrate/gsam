e8106ef841ada21290a131ff2b62a480c0d4f3f8
yhoogstrate <y.hoogstrate@erasmusmc.nl>
2020-09-25 09:37:10 +0200

    update to ensembl gene id with new identifiers

--- scripts/R/ensembl_to_geneid.R
+++ scripts/R/ensembl_to_geneid.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env R
 
-#ensembl_to_geneid <- function() {
-#  d <- read.table("/home/youri/bio/gxf/gencode.v29.annotation.gff3",sep="\t",comment.char="#",stringsAsFactors = F)
+# ensembl_to_geneid <- function() {
+#  d <- read.table("/mnt/data/ccbc_environment/project/gsam-neurology/ref/star-hg19/gencode.v31lift37.annotation.gtf",sep="\t",comment.char="#",stringsAsFactors = F)
 #  d$V2 <- NULL
 #  d$V6 <- NULL
 #  d$V7 <- NULL
@@ -11,17 +11,17 @@
 #  d$ens.id <- gsub(';.+','',gsub('.+gene_id=','',d$V9))
 #  d$V9 <- NULL
 #  d$gene.id <- paste(d$gene.id,"|",d$V1,":",round(((d$V4 + d$V5) / 2)/1000000),"M",sep="")
-#  
+# 
 #  d$V1 <- NULL
 #  d$V3 <- NULL
 #  d$V4 <- NULL
 #  d$V5 <- NULL
 #  rownames(d) <- NULL
-#  
+# 
 #  ens_ids <- d
 #  rm(d)
 #  save(ens_ids, file="tmp/ens_ids.RData")
-#}
+# }
 
 load("tmp/ens_ids.RData")
 
