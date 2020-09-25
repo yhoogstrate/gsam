9b703ec1bf49e06a54a373eca660b7a57f9ed3e9
yhoogstrate <y.hoogstrate@erasmusmc.nl>
2020-09-25 10:47:28 +0200

    sav

--- sync-data-to-neuro-genomic-1.sh
+++ sync-data-to-neuro-genomic-1.sh
@@ -5,5 +5,8 @@ rsync -avP --exclude ".*" --no-links 'output/' 'gsamadmin@neuro-genomic-1.erasmu
 rsync -avP --exclude ".*" --exclude "__pycache__"  --no-links 'scripts/' 'gsamadmin@neuro-genomic-1.erasmusmc.nl:/volume1/gsam/scripts/'
 rsync -avP --exclude ".*" --exclude "__pycache__"  --no-links 'documents/' 'gsamadmin@neuro-genomic-1.erasmusmc.nl:/volume1/gsam/documents/'
 
+rsync -avP --exclude ".*" --exclude "*.fa" --exclude "SAindex" --exclude "__pycache__" --exclude "SA" --exclude "Genome" --exclude "rsem-hg19/" --no-links 'ref/' 'gsamadmin@neuro-genomic-1.erasmusmc.nl:/volume1/gsam/ref/'
+
+# use -n for dry run
 
 
