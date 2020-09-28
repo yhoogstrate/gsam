00a13ad4799c7cb7397bdb010f53798f630b26c0
yhoogstrate <y.hoogstrate@erasmusmc.nl>
2020-09-28 16:44:02 +0200

    sav

--- sync-data-to-neuro-genomic-1.sh
+++ sync-data-to-neuro-genomic-1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #sudo mount /mnt/neurogen-rw/egfr-viii-determination
-rsync -avP --exclude ".*" --no-links 'output/' 'gsamadmin@neuro-genomic-1.erasmusmc.nl:/volume1/gsam/output/'
+#rsync -avP --exclude ".*" --no-links 'output/' 'gsamadmin@neuro-genomic-1.erasmusmc.nl:/volume1/gsam/output/'
+rsync -avP --exclude ".*" --exclude "output/alignments" --exclude "alignments/" --exclude "tables/rseqc/" --exclude "rseqc/" --no-links 'output/' 'gsamadmin@neuro-genomic-1.erasmusmc.nl:/volume1/gsam/output/'
 rsync -avP --exclude ".*" --exclude "__pycache__"  --no-links 'scripts/' 'gsamadmin@neuro-genomic-1.erasmusmc.nl:/volume1/gsam/scripts/'
 rsync -avP --exclude ".*" --exclude "__pycache__"  --no-links 'documents/' 'gsamadmin@neuro-genomic-1.erasmusmc.nl:/volume1/gsam/documents/'
 
