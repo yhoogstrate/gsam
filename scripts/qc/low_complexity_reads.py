4c5863cbab04780ca273d4369e52d5dbe944401d
yhoogstrate <y.hoogstrate@erasmusmc.nl>
2020-09-28 16:38:23 +0200

    update

--- scripts/qc/low_complexity_reads.py
+++ scripts/qc/low_complexity_reads.py
@@ -3,15 +3,28 @@
 import os
 from tqdm import tqdm
 import json
+import re
 
 
-path = 'data/RNA/alignments_log'
+path = 'data/RNA/output/tables/fastp_log'
 
 
-with open("output/tables/low_complexity_reads.txt", "w") as fh_w:
+with open("output/tables/qc/low_complexity_reads.txt", "w") as fh_w:
     fh_w.write("sample\tpercentage low complexity reads\n")
 
     for fn in tqdm([_ for _ in os.listdir(path) if _[-5:] == ".json"]):
+        if fn.find("NGS19") != -1:
+            sid = fn.split("_NGS19")[0]
+            sid = re.sub("\\-[0-9]{3,}","", sid)
+        elif fn.find("NGS20") != -1:
+            sid = fn.split("_NGS20")[0].split("-", 1)[1]+"-new"
+        else:
+            print(fn)
+            import sys
+            sys.exit(1)
+
+        print(sid)
+
         with open(os.path.join(path, fn), 'r') as fh:
             c = fh.read()
             j = json.loads(c)
@@ -22,5 +35,6 @@ with open("output/tables/low_complexity_reads.txt", "w") as fh_w:
 
 
             #print(str(lc) + "/" + str(total))
-            fh_w.write(fn.split("_")[1] + "\t" + str(lc_p) + "%"+"\n")
+            fh_w.write(sid + "\t" + str(lc_p) + "%"+"\n")
+
 
--- scripts/qc/low_complexity_reads_plot.R
+++ scripts/qc/low_complexity_reads_plot.R
@@ -1,33 +1,53 @@
 #!/usr/bin/env R
 
-setwd("/home/youri/projects/gsam")
+# ---- load cfg -----
 
-d <- read.delim("output/tables/low_complexity_reads.txt", header=T, stringsAsFactors = F)
-d$percentage_numerical <- as.numeric(gsub("%","",d$percentage.low.complexity.reads,fixed=T))
-e <- d[order(d$percentage_numerical),]
+setwd("~/projects/gsam")
 
-png("output/figures/low_complexity_reads.png",width=480*3,height=1.7*480,res=72*1.8)
-plot(c(1, max(nrow(e))) , c(0, max(e$percentage_numerical) + 1), main="Percentage low-complexity discarded reads",type="n", xlab="Sample nr. / order", ylab = "Percentage reads discarted due to low complexity")
+# ---- load libs ----
+
+library(tidyverse)
+
+# ---- load data ----
+
+plt <- read.delim("data/output/tables/qc/low_complexity_reads.txt", header=T, stringsAsFactors = F) %>%
+  dplyr::mutate(percentage_numerical = as.numeric(gsub("%","", .$percentage.low.complexity.reads,fixed=T))) %>%
+  dplyr::arrange(percentage_numerical) %>%
+  dplyr::mutate(new.batch = grepl("new", sample)) %>%
+  dplyr::mutate(name.strip = ifelse(new.batch == T ,gsub("-new", "", .$sample, fixed=T),"") ) %>%
+  dplyr::mutate(old.batch = sample %in% name.strip) %>%
+  dplyr::mutate(name.strip = NULL) %>%
+  dplyr::mutate(batch = dplyr::case_when ((old.batch == T & new.batch == F) ~ "old" , (old.batch == F & new.batch == T) ~ "new" , (old.batch == F & new.batch == F) ~ "single" )) %>%
+  dplyr::mutate(batch = as.factor(batch))
+
+# ---- plot 1 base R ----
+
+#png("output/figures/low_complexity_reads.png",width=480*3,height=1.7*480,res=72*1.8)
+plot(c(1, max(nrow(e))) , c(0, max(e$percentage_numerical) + 1), main="Percentage low-complexity discarded reads",type="n", xlab="Sample nr. / order", ylab = "Percentage reads discarted due to low complexity",
+     col = as.numeric(grepl("new", e$sample)) + 1)
 for(i in 1:nrow(e)) {
   print(i)
   lines(c(i,i), c(0,e[i,]$percentage_numerical),col="darkgray",lty=1)
-  text(i,e[i,]$percentage_numerical + 0.5, e[i,]$sample, srt=90, pos=3,cex=0.6)
+  text(i - 3,e[i,]$percentage_numerical + 0.25, e[i,]$sample, srt=90, pos=4,cex=0.6,
+      col = as.numeric(grepl("new", e$sample)) + 1 )
 }
-
-points(1:nrow(e) , e$percentage_numerical, pch=19,cex=0.6)
-dev.off()
-
+points(1:nrow(e) , e$percentage_numerical, pch=19,cex=0.6, col = as.numeric(grepl("new", e$sample)) + 1 )
+#dev.off()
 
 
 
-#g <- data.frame(
-#  x = 1:nrow(e),
-#  y_max = e$percentage_numerical,
-#  y_min = rep(0, nrow(e))
-#  )
+# ---- plot 1 gg ----
 
-#library(ggplot2)
-#ggplot(g, aes(x=x, y=y_max)) + geom_point(shape=1) 
+ggplot(plt, aes(x=reorder(sample, percentage_numerical), fill = batch, y=percentage_numerical, label=sample)) +
+  geom_point(col = "black",pch=21) +
+  ggrepel::geom_text_repel(aes(col=batch),srt = 90, data=subset(plt, batch !="single"))
+
+
+
+
+
+
+
 
 
 
