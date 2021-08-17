56ce8f31216d1d54f01c973a3be93c3889c73959
yhoogstrate <y.hoogstrate@gmail.com>
2021-08-17 14:57:23 +0200

    sav

--- scripts/R/cor_cor_plot.R
+++ scripts/R/cor_cor_plot.R
@@ -7,16 +7,14 @@ library(ggforce)
 
 
 
-cor_cor_plot <- function(normalised_correlated_data) {
-  normalised_correlated_data <- tmp.bak
+cor_cor_plot <- function(normalised_correlated_data, labels) {
+  
+  normalised_correlated_data <- plt.n
   
   # remove duplicate entries:
   plt <- normalised_correlated_data %>%
     tibble::rownames_to_column('__hugo_symbol__') %>%
     dplyr::filter(!duplicated(`__hugo_symbol__`)) %>%
-    dplyr::mutate(`__hugo_symbol__` = gsub("^ENSG[0-9\\._]+","", `__hugo_symbol__`)) %>%
-    dplyr::mutate(`__hugo_symbol__` = gsub("chr.+$","", `__hugo_symbol__`)) %>%
-    dplyr::mutate(`__hugo_symbol__` = gsub('|','', `__hugo_symbol__`,fixed=T)) %>%
     tibble::column_to_rownames('__hugo_symbol__')
   
   # determine correlation
@@ -66,20 +64,19 @@ cor_cor_plot <- function(normalised_correlated_data) {
          aes( x0 = x.order, y0 = y.order, 
              r = ((abs(value) * 0.7) + 0.3) / 2 - 0.05,  # [0.3 , 0.8] + 0.2 smoothened from lwd/border
              fill=value,
-             col=value
-             )
+             col=value,
+             label=x)
          ) +
     geom_tile(aes(  x = x.order, y = y.order,), col="gray", fill="white", lwd=0.3) +
     scale_fill_gradientn( colours = col2(200), na.value = "grey50", limits = c(-1,1) , guide="none") + # guide = "colourbar",
     scale_color_gradientn( colours = col2(200), na.value = "grey50", limits = c(-1,1) , guide="none" ) +
     geom_circle(lwd=0.2) +
-
     scale_x_discrete(labels = NULL, breaks = NULL) +
-    labs(x = "") +
-    
     theme(legend.position = 'bottom',
-          axis.text = element_text(),
-          text = element_text(size=5),
+          axis.text.y = element_text(size = 9, angle = 0, hjust = 1, vjust = 0.5),
+          axis.text.x = element_text(angle = 90, hjust = 0, vjust = 0.5, color="gray80"),
+          
+          text = element_text(size=13),
           
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(),
@@ -88,25 +85,35 @@ cor_cor_plot <- function(normalised_correlated_data) {
           ) +
     labs(y = NULL, x=NULL, main=NULL) +
     ggplot2::coord_fixed() +
-    scale_y_continuous(name=NULL, breaks = 1:length(o), labels = o)
+    scale_y_continuous(name=NULL, breaks = length(o):1, labels = o)
   
   
   
   plt <- data.frame(gid = o, i = 1:length(o)) %>%
-    dplyr::left_join(labels.bak %>% tibble::rownames_to_column('gid'), by=c('gid' = 'gid')) %>%
+    dplyr::left_join(labels %>% tibble::rownames_to_column('gid'), by=c('gid' = 'gid')) %>%
     reshape2::melt(id.vars = c('gid','i'))  
   
   
-  p2 <- ggplot(plt, aes(x = reorder(gid, -i) , y = variable, fill=value, label=gid)) +
+  p2 <- ggplot(plt, aes(x = i , y = variable, fill=value, label=gid)) +
     geom_tile(col='white',lwd=0.6) +
-    scale_x_discrete(position = "top")  + 
-    theme(axis.text.x = element_text(angle = 90, hjust = 0, vjust = 0.5)) +
+    #scale_x_discrete(position = "bottom")  + 
+    scale_x_discrete(labels = NULL, breaks = NULL) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 0, vjust = 0.5),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(),
+          axis.line = element_blank()
+          ) +
     guides(fill="none") +
     ggplot2::coord_fixed() +
-    labs(x=NULL, y=NULL)
+    labs(x=NULL, y=NULL) + 
+    scale_fill_manual(values=c('TRUE'='gray40','FALSE'='gray95')) 
+    #xlim(1, length(o) )
 
   
   p2 / p1
+  
+  
 }
 
 
