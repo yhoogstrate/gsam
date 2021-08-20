82ac81ced9390247c59ab76727b5086c70ef7726
yhoogstrate <y.hoogstrate@gmail.com>
2021-08-20 16:32:08 +0200

    GGCORRPLOTRELOADED

--- scripts/R/cor_cor_plot.R
+++ scripts/R/cor_cor_plot.R
@@ -1,10 +1,21 @@
 #!/usr/bin/env R
 
 
+setwd("~/projects/gsam")
+
+
 library(ggplot2)
 library(tidyverse)
-library(ggforce)
+#library(ggforce)
 
+rm(geomnet)
+rm(geom_circle)
+detach("package:geomnet")
+detach("package:geomnet", unload=TRUE)
+detach("function:geom_circle", unload=TRUE)
+remove.packages('geomnet')
+devtools::install_github("yhoogstrate/geomnet")
+library(geomnet)
 
 
 
@@ -70,16 +81,16 @@ cor_cor_plot <- function(normalised_correlated_data, labels) {
   
   
   p1 <- ggplot(plt.expanded2,
-         aes( x0 = x.order, y0 = y.order, 
-             r = ((abs(value) * 0.7) + 0.3) / 2 - 0.05,  # [0.3 , 0.8] + 0.2 smoothened from lwd/border
+         aes( x = x.order, y = y.order, 
+             radius = ((abs(value) * 0.7) + 0.3) / 2 - 0.05 ,  # [0.3 , 0.8] + 0.2 smoothened from lwd/border
              fill=value,
              col=value,
              label=x)
          ) +
-    geom_tile(aes(  x = x.order, y = y.order,), col="gray", fill="white", lwd=0.3) +
+    geom_tile( col="gray", fill="white", lwd=0.3) +
     scale_fill_gradientn( colours = col2(200), na.value = "grey50", limits = c(-1,1) , guide="none") + # guide = "colourbar",
     scale_color_gradientn( colours = col2(200), na.value = "grey50", limits = c(-1,1) , guide="none" ) +
-    geom_circle(lwd=0.2) +
+    geomnet::geom_circle(radius.fixed = T) + # not to be confused w/ geomnet::geom_circle
     scale_x_discrete(labels = NULL, breaks = NULL) +
     theme(legend.position = 'bottom',
           axis.text.y = element_text(size = 9, angle = 0, hjust = 1, vjust = 0.5),
@@ -126,9 +137,29 @@ cor_cor_plot <- function(normalised_correlated_data, labels) {
 }
 
 
-#cor_cor_plot(expression_vst_data , labels)
+cor_cor_plot(plt, labels)
 
 
+
+
+# plt.expanded2 <- plt.expanded2 %>%
+#   dplyr::mutate(r = ((abs(value) * 0.7) + 0.3) / 2 - 0.05) %>%
+#   dplyr::mutate(r = r / 15)
+
+
+
+#saveRDS(plt.expanded2 , file="tmp/tmp.Rds")
+plt.expanded2  <- readRDS(file="tmp/tmp.Rds")
+
+# , 
+# fill=value,
+# col=value,
+# label=x
+
+plt <- plt.expanded2 %>% dplyr::slice_head(n=2)
+
+ggplot(plt, aes( x = x.order, y = y.order, radius = r)) +
+  geom_circle(radius.fixed = T) # not to be confused w/ geomnet::geom_circle
 
 
 
