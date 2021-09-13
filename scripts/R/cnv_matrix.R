5ee213bc4bd6f20e826b639272cf5a02bd8d7560
yhoogstrate <y.hoogstrate@gmail.com>
2021-09-13 08:56:15 +0200

    update

--- scripts/R/cnv_matrix.R
+++ scripts/R/cnv_matrix.R
@@ -2,21 +2,41 @@
 
 # load ----
 
-cnv_matrix <- read.table("data/gsam/output/tables/cnv_copynumber-ratio.cnr_log2_all.txt",stringsAsFactors = F,header=T)
+cnv_matrix <- read.table("data/gsam/output/tables/cnv_copynumber-ratio.cnr_log2_all.txt",stringsAsFactors = F,header=T) %>%
+  dplyr::rename(chr = chromosome) %>%
+  dplyr::mutate(chr = paste0('chr', chr) ) %>%
+  dplyr::mutate(loc = paste0(chr,":",start, "-", end)) %>%
+  dplyr::arrange(loc)
+
+
+
+#cnv_matrix[ncol(cnv_matrix)-4:ncol(cnv_matrix)]
+
+
 
 ## make a separate table for the gene information ----
-cnv_matrix_genes <- cnv_matrix[,1:4]
-cnv_matrix_genes$chr <- paste0('chr',cnv_matrix_genes$chromosome)
-cnv_matrix_genes$loc <- paste0(cnv_matrix_genes$chr,":",cnv_matrix_genes$start, "-", cnv_matrix_genes$end)
-cnv_matrix <- cnv_matrix[,-(1:4)] # exclude gene details
-rownames(cnv_matrix) <- cnv_matrix_genes$loc
-
-## reorder by sorted colnames ----
-cnv_matrix <- cnv_matrix[,order(colnames(cnv_matrix))]
-
+cnv_matrix_genes <- cnv_matrix %>%
+  dplyr::select(c('chr','start','end','gene', 'loc')) %>%
+  tibble::column_to_rownames('loc')
+  
+cnv_matrix <- cnv_matrix  %>%
+  dplyr::select( - c('chr','start','end','gene')) %>%
+tibble::column_to_rownames('loc')
 
 
-# remove rows with NA values
+
+#cnv_matrix[ncol(cnv_matrix)-4:ncol(cnv_matrix)]
+
+
+
+stopifnot(rownames(cnv_matrix) == rownames(cnv_matrix_genes))
+
+
+## 
+
+
+# remove rows with NA values ----
+
 naPerRow <- apply(cnv_matrix, 1, function(tmp) sum(is.na(tmp)))
 tmp <- nrow(cnv_matrix)
 sel <- naPerRow <= 4
@@ -26,16 +46,21 @@ print(paste0("Removal of ",tmp - nrow(cnv_matrix) , " CNV regions only present i
 
 
 
-# remove columns with NA values
+# remove columns with NA values ----
+
 naPerCol <- apply(cnv_matrix, 2, function(x) sum(is.na(x)))
 tmp <- ncol(cnv_matrix)
 cnv_matrix <- cnv_matrix[,naPerCol == 0]
 print(paste0("Removal of ",tmp - ncol(cnv_matrix) , " CNV regions due to missing/corrupt and excluded files" ))
 stopifnot(sum(is.na(head(cnv_matrix))) == 0) # no NA values may exist
 
+
+# add check on colnames ----
+
 colnames(cnv_matrix) <- gsub("(.b1|.b2)$","",colnames(cnv_matrix))
 
-# change PD ids (sequencing ids) to study ids
+
+# change PD ids (sequencing ids) to study ids ----
 if(!exists('gsam.cnv.metadata')) {
   source("scripts/R/gsam_metadata.R")
 }
@@ -46,10 +71,13 @@ tmp <- match(colnames(cnv_matrix), gsam.cnv.metadata$PD_ID) # order
 stopifnot(sum(is.na(tmp)) == 0)
 colnames(cnv_matrix) <- gsam.cnv.metadata[tmp,]$donor_ID
 
-# reorder to match metadata order
-cnv_matrix <- cnv_matrix[,match(gsam.cnv.metadata$donor_ID , colnames(cnv_matrix))]
 
-rm(tmp, naPerRow, naPerCol, sel)
+# reorder to match metadata order
+#cnv_matrix <- cnv_matrix[,match(gsam.cnv.metadata$donor_ID , colnames(cnv_matrix))]
+#rm(tmp, naPerRow, naPerCol, sel)
+
 
 
 # 〰 © Dr. Youri Hoogstrate 〰 ----
+
+
