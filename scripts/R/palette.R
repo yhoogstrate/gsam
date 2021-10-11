6062da511a71b3a3b33a8f07ae48d371e1ec18bf
yhoogstrate <y.hoogstrate@gmail.com>
2021-10-11 15:42:28 +0200

    sav

--- scripts/R/palette.R
+++ scripts/R/palette.R
@@ -15,5 +15,13 @@ subtype_colors_nmfm <- c('NMF meta-feature 2'='#6ba6e5',# blue
                          'NMF meta-feature 1'='#eab509',#mustard
                          'NMF meta-feature 3'='#ff5f68')#red/pink
 
+dataset_colors <- c('G-SAM' = '#e69356', 'GSAM' = '#e69356', 'GLASS' = '#69a4d5')
 
+resection_colors <- c(
+  "Primary Res." = "#55864e",
+  "R1" = "#55864e" ,
+  
+  "Recurrent Res." = "#a15e5e",
+  "R2" = "#a15e5e"  # #a1805e
+)
 
