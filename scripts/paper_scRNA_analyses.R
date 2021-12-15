338c5c0217e11f9d0f351eb82544d3ce127c8658
yhoogstrate <y.hoogstrate@gmail.com>
2021-12-15 12:34:11 +0100

    update scrna

--- scripts/paper_scRNA_analyses.R
+++ scripts/paper_scRNA_analyses.R
@@ -3918,9 +3918,94 @@ FeaturePlot(object = object_1, features = C6)
 
 
 
+## BT368-GSC [95-100% tumor] ----
 
 
-## BT368 <geen scheiding, per pericytes, vraag levi voor hulp?> ----
+rm(object_1)
+gc()
+
+sid <- "BT368-GSC.filtered_gene_matrices"
+object_1 <- Read10X(data.dir = paste0("data/scRNA/EGAS00001004422_Couturier/filtered/",sid,"/"))
+object_1 <- CreateSeuratObject(counts = object_1, min.cells = 3, min.features = 200, project="Couturier")
+
+mito.features_object1 <- grep(pattern = "^MT-", x=rownames(x=object_1), value=T)
+percent.mito_object1 <- Matrix::colSums(x = GetAssayData(object = object_1, slot="counts")[mito.features_object1,]) / Matrix::colSums(x = GetAssayData(object = object_1, slot = "counts"))
+object_1[["percent.mito"]] <- percent.mito_object1
+VlnPlot(object = object_1, features = c("nFeature_RNA", "nCount_RNA", "percent.mito"), ncol = 3, pt.size = 0.01, group.by = "orig.ident") 
+
+
+ggplot(object_1@meta.data, aes(y=`nFeature_RNA`, x=orig.ident)) +
+  geom_jitter(cex=0.01) +
+  geom_hline(yintercept = 300,col="red") +
+  geom_hline(yintercept = 4500,col="red")
+
+
+ggplot(object_1@meta.data, aes(y=`nCount_RNA`, x=orig.ident)) +
+  geom_jitter(cex=0.01)  +
+  geom_hline(yintercept = 500,col="red") +
+  geom_hline(yintercept = 20000,col="red") # + scale_y_log10()
+
+
+object_1 <- subset(x = object_1, subset =
+                     nFeature_RNA > 300 &
+                     nFeature_RNA < 4500 &
+                     nCount_RNA > 500 &
+                     nCount_RNA < 20000 &
+                     percent.mito < 0.2)
+
+
+object_1 <- NormalizeData(object = object_1, normalization.method = "LogNormalize", scale.factor = 1e4)
+object_1 <- FindVariableFeatures(object = object_1, selection.method = "vst", nfeatures = 2000)
+object_1[["state"]] <- "P1" 
+
+
+top10 <- head(VariableFeatures(object_1), 10)
+
+
+# plot variable features with and without labels
+
+plot1 <- VariableFeaturePlot(object_1)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+#CombinePlots(plots = list(plot1, plot2))     
+plot1
+plot2
+
+
+#Shifts the expression of each gene, so that the mean expression across cells is 0
+#Scales the expression of each gene, so that the variance across cells is 1
+#This step gives equal weight in downstream analyses, so that highly-expressed genes do not dominate
+#The results of this are stored in pbmc[["RNA"]]@scale.data
+
+all.genes <- rownames(object_1)
+object_1 <- ScaleData(object_1, features = all.genes)
+
+
+object_1 <- RunPCA(object_1, features = VariableFeatures(object = object_1))
+print(object_1[["pca"]], dims = 1:5, nfeatures = 5)
+VizDimLoadings(object_1, dims = 1:2, reduction = "pca")
+DimPlot(object_1, reduction = "pca")
+
+#### estimation of the number of principle components in your dataset
+
+ElbowPlot(object_1, ndims = 45)
+
+object_1 <- FindNeighbors(object_1, dims = 1:30)
+object_1 <- FindClusters(object_1, resolution = 1, algorithm=1)
+head(Idents(object_1), 20)
+
+### UMAP clustering ----
+
+object_1 <- RunUMAP(object_1, dims = 1:30)
+object_1@meta.data$pt = sapply(strsplit(rownames(object_1@meta.data), "[.]"), "[", 1)
+
+# levels(object_1$seurat_clusters) <- gsub("^(14)$",paste0("TAM/microglia"),levels(object_1$seurat_clusters))
+
+DimPlot(object_1, reduction = "umap", label = TRUE, pt.size = .6, group.by = "seurat_clusters")
+
+
+
+
+## BT368 Total <geen scheiding, per pericytes, vraag levi voor hulp?> ----
 
 rm(object_1)
 gc()
@@ -4100,7 +4185,7 @@ FeaturePlot(object = object_1, features = "GABRB2")
 
 FeaturePlot(object = object_1, features = "TMEM144")
 FeaturePlot(object = object_1, features = "MOG")
-FeaturePlot(object = object_1, features = "OPALIN")
+FeaturePlot(object = object_1, features = "TMEM125")
 FeaturePlot(object = object_1, features = "PLP1")
 
 
@@ -4155,86 +4240,6 @@ VlnPlot(object = object_1, features = c(f), group.by = "seurat_clusters",stack=T
 
 
 FeaturePlot(object = object_1, features = C6)
-
-
-## BT368-GSC ----
-
-sid <- "BT368-GSC.filtered_gene_matrices"
-object_1 <- Read10X(data.dir = paste0("data/scRNA/EGAS00001004422_Couturier/filtered/",sid,"/"))
-object_1 <- CreateSeuratObject(counts = object_1, min.cells = 3, min.features = 200, project="Couturier")
-
-mito.features_object1 <- grep(pattern = "^MT-", x=rownames(x=object_1), value=T)
-percent.mito_object1 <- Matrix::colSums(x = GetAssayData(object = object_1, slot="counts")[mito.features_object1,]) / Matrix::colSums(x = GetAssayData(object = object_1, slot = "counts"))
-object_1[["percent.mito"]] <- percent.mito_object1
-VlnPlot(object = object_1, features = c("nFeature_RNA", "nCount_RNA", "percent.mito"), ncol = 3, pt.size = 0.01, group.by = "orig.ident") 
-
-
-ggplot(object_1@meta.data, aes(y=`nFeature_RNA`, x=orig.ident)) +
-  geom_jitter(cex=0.01) +
-  geom_hline(yintercept = 300,col="red") +
-  geom_hline(yintercept = 4500,col="red")
-
-ggplot(object_1@meta.data, aes(y=`nCount_RNA`, x=orig.ident)) +
-  geom_jitter(cex=0.01)  +
-  geom_hline(yintercept = 500,col="red") +
-  geom_hline(yintercept = 20000,col="red") # + scale_y_log10()
-
-
-object_1 <- subset(x = object_1, subset =
-                     nFeature_RNA > 300 &
-                     nFeature_RNA < 4500 &
-                     nCount_RNA > 500 &
-                     nCount_RNA < 20000 &
-                     percent.mito < 0.2)
-
-
-object_1 <- NormalizeData(object = object_1, normalization.method = "LogNormalize", scale.factor = 1e4)
-object_1 <- FindVariableFeatures(object = object_1, selection.method = "vst", nfeatures = 2000)
-object_1[["state"]] <- "P1" 
-
-
-top10 <- head(VariableFeatures(object_1), 10)
-
-
-# plot variable features with and without labels
-
-plot1 <- VariableFeaturePlot(object_1)
-plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
-#CombinePlots(plots = list(plot1, plot2))     
-plot1
-plot2
-
-
-#Shifts the expression of each gene, so that the mean expression across cells is 0
-#Scales the expression of each gene, so that the variance across cells is 1
-#This step gives equal weight in downstream analyses, so that highly-expressed genes do not dominate
-#The results of this are stored in pbmc[["RNA"]]@scale.data
-
-all.genes <- rownames(object_1)
-object_1 <- ScaleData(object_1, features = all.genes)
-
-
-object_1 <- RunPCA(object_1, features = VariableFeatures(object = object_1))
-print(object_1[["pca"]], dims = 1:5, nfeatures = 5)
-VizDimLoadings(object_1, dims = 1:2, reduction = "pca")
-DimPlot(object_1, reduction = "pca")
-
-#### estimation of the number of principle components in your dataset
-
-ElbowPlot(object_1, ndims = 45)
-
-object_1 <- FindNeighbors(object_1, dims = 1:30)
-object_1 <- FindClusters(object_1, resolution = 1, algorithm=1)
-head(Idents(object_1), 20)
-
-### UMAP clustering ----
-
-object_1 <- RunUMAP(object_1, dims = 1:30)
-object_1@meta.data$pt = sapply(strsplit(rownames(object_1@meta.data), "[.]"), "[", 1)
-
-# levels(object_1$seurat_clusters) <- gsub("^(14)$",paste0("TAM/microglia"),levels(object_1$seurat_clusters))
-
-DimPlot(object_1, reduction = "umap", label = TRUE, pt.size = .6, group.by = "seurat_clusters")
 
 
 
